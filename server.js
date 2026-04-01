af3af2c2072d83bae8159e68e97b13b92e36f571
Manu Barki <manulb9@gmail.com>
2026-04-01 20:11:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import fetch from "node-fetch";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
